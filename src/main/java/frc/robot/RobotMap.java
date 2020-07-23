ae64122d42d7606bbc1e8972bb1e8ea2021fd30b
JHG777000 <jhg777000@gmail.com>
2020-07-23 13:11:49 -0400

    Remove import in RobotMap.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
